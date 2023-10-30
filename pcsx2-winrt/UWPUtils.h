de1422bede918eedb582b38f7b882a3a50c69b40
0x61 <ad-min@mailbox.org>
2023-10-30 23:06:24 +0100

    revert

--- pcsx2-winrt/UWPUtils.h
+++ pcsx2-winrt/UWPUtils.h
@@ -12,29 +12,6 @@ namespace UWP
 {
 	inline std::string GetLocalFolder()
 	{
-		using namespace winrt::Windows::Storage;
-		
-		try
-		{
-			auto externalFolderPath = L"E:\\xbsx2";
-			
-			// Get the StorageFolder for the drive
-			auto storageFolder = StorageFolder::GetFolderFromPathAsync(externalFolderPath).get();
-
-			// Check if the folder exists on the drive
-			if (storageFolder) 
-			{
-				// E:\xbsx2 exists, return its path
-				return winrt::to_string(externalFolderPath);
-			}
-		}
-		catch (const winrt::hresult_error& ex)
-		{
-			// Handle specific error (ex.message() contains the error message)
-			// For now, we'll just ignore the exception and proceed to the fallback path.
-		}
-
-		// If E:\xbsx2 doesn't exist or if there's an exception, fall back to default LocalFolder path.
 		return winrt::to_string(winrt::Windows::Storage::ApplicationData::Current().LocalFolder().Path());
 	}
 }
